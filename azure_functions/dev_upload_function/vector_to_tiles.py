082872e1ac9d8a34c87783f53cfba6485d79d073
Jesse Friend <friendjessef@gmail.com>
2023-02-15 11:19:30 +0100

    Tippecanoe to felt src, vector conversion cmd

--- azure_functions/dev_upload_function/vector_to_tiles.py
+++ azure_functions/dev_upload_function/vector_to_tiles.py
@@ -1,2 +1,36 @@
+import subprocess
+
+
 def vector_process(filename):
-    return
+    subprocess.run(
+        [
+            "ogr2ogr",
+            "-f",
+            "GeoJSONSeq",
+            filename,
+            "|",
+            "tippecanoe",
+            "-n",
+            filename,
+            "-A",
+            "attribution, license",
+            "-N",
+            "description of dataset",
+            "-zg",
+            "--simplify-only-low-zooms",
+            "--detect-shared-borders",
+            "--read-parallel",
+            "--no-tile-size-limit",
+            "--no-tile-compression",
+            "--force",
+            "-o",
+            "test.pmtiles",
+            "-L",
+            {
+                "file": "test.geojson",
+                "layer": "layer name",
+                "description": "layer description",
+            },
+        ],
+        stdout=subprocess.DEVNULL,
+    )
