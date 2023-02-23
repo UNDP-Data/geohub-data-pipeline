6c45e1ea76baa5a20bd08c788fa06ae4f07e627e
Jesse Friend <friendjessef@gmail.com>
2023-02-23 11:20:04 +0100

    Last processing to ingesting refactor

--- app/ingestion.py
+++ app/ingestion.py
@@ -8,23 +8,23 @@ from .utils import prepare_file
 from .vector_to_tiles import vector_ingest
 
 
-def process(filename):
-    logging.info(f"Processing {filename}")
+def ingest(filename):
+    logging.info(f"Ingesting {filename}")
     raster_layers, vector_layers, local_path = prepare_file(filename)
 
     if not raster_layers or vector_layers:
         logging.error("File is not readable in a GIS")
 
     if raster_layers > 0 and vector_layers > 0:
-        logging.info("File contains both raster and vector data, beginning processing")
+        logging.info("File contains both raster and vector data, beginning ingesting")
         return "both"
 
     elif raster_layers > 0:
-        logging.info("File contains raster data, beginning processing")
+        logging.info("File contains raster data, beginning ingesting")
         raster_ingest(local_path)
 
     elif vector_layers > 0:
-        logging.info("File contains vector data, beginning processing")
+        logging.info("File contains vector data, beginning ingesting")
         vector_ingest(local_path)
 
     else:
