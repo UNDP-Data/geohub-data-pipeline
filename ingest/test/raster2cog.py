c1865cc7c8a28f0ee09ee94880d98e05c884fb30
janf <ioan.ferencik@solargis.com>
2023-03-28 11:46:55 +0200

    added timing function for linux

--- ingest/test/raster2cog.py
+++ ingest/test/raster2cog.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import time
-
+import os
 import rasterio
 from osgeo import gdal
 from rio_cogeo import cog_info
@@ -9,6 +9,54 @@ from rio_cogeo.cogeo import cog_translate
 from rio_cogeo.profiles import cog_profiles
 
 logger = logging.getLogger(__name__)
+
+def flush_cache(passwd):
+    """
+    Flush Linux VM caches. Useful for doing meaningful tmei measurements for
+    NetCDF or similar libs.
+    Needs sudo password
+    :return: bool, True if success, False otherwise
+    """
+    logger.debug('Clearing the OS cache using sudo -S sh -c "sync; echo 3 > /proc/sys/vm/drop_caches')
+    #ret = os.system('echo %s | sudo -S sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"' % passwd)
+    ret = os.popen('sudo -S sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"', 'w').write(passwd)
+    return not bool(ret)
+
+def timeit(func=None,loops=1,verbose=False, clear_cache=False, sudo_passwd=None):
+    #print 0, func, loops, verbose, clear_cache, sudo_passwd
+    if func != None:
+        if clear_cache:
+            assert sudo_passwd, 'sudo_password argument is needed to clear the kernel cache'
+
+        def inner(*args,**kwargs):
+            sums = 0.0
+            mins = 1.7976931348623157e+308
+            maxs = 0.0
+            logger.debug('====%s Timing====' % func.__name__)
+            for i in range(0,loops):
+                if clear_cache:
+                    flush_cache(sudo_passwd)
+                t0 = time.time()
+                result = func(*args,**kwargs)
+                dt = time.time() - t0
+                mins = dt if dt < mins else mins
+                maxs = dt if dt > maxs else maxs
+                sums += dt
+                if verbose == True:
+                    logger.debug('\t%r ran in %2.9f sec on run %s' %(func.__name__,dt,i))
+            logger.debug('%r min run time was %2.9f sec' % (func.__name__,mins))
+            logger.debug('%r max run time was %2.9f sec' % (func.__name__,maxs))
+            logger.info('%r avg run time was %2.9f sec in %s runs' % (func.__name__,sums/loops,loops))
+            logger.debug('==== end ====')
+            return result
+
+        return inner
+    else:
+        def partial_inner(func):
+            return timeit(func,loops,verbose, clear_cache, sudo_passwd)
+        return partial_inner
+
+
 
 
 def ingest_raster():
@@ -119,7 +167,7 @@ def gdal_configs(config={}, profile="zstd"):
     # config["GDAL_TIFF_OVR_BLOCKSIZE"] = "128"
     config[
         "CPL_VSIL_USE_TEMP_FILE_FOR_RANDOM_WRITE"
-    ] = "YES"  # necessary to write files to AZ directkly using rio
+    ] = "YES"  # necessary to write files to AZ directly using rio
 
     return config, output_profile
 
