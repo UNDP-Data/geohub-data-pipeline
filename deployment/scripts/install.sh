eb9ddb7cba6c9e4fe2704091e74e27704bd5b9a6
iferencik <92793531+iferencik@users.noreply.github.com>
2023-10-06 17:57:36 +0200

    Fix/kube idle replica count (#38)
    * adjust minreplicacount
    
    * remove skipfailures
    
    * improve projection check function
    
    * change deployment restart policy to avoid CrashLoopbackOff
    
    * change deployment restart policy to avoid CrashLoopbackOff
    
    * polish kube deployment
    
    * replace deployment and ScaledObject with KEDA ScaledJob
    
    ---------
    
    Co-authored-by: janf <ioan.ferencik@solargis.com>

--- deployment/scripts/install.sh
+++ deployment/scripts/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Source the .env file located in the same directory as the script
-. ./.env
-
+. ../.env
+# Rest of the script
+kubectl apply -f ../yaml/ingest-environment_new.yaml
 # create secret with environmental variables
 kubectl create secret generic ingest-secrets \
 --from-literal=AZURE_STORAGE_CONNECTION_STRING=$AZURE_STORAGE_CONNECTION_STRING \
 --from-literal=SERVICE_BUS_CONNECTION_STRING=$SERVICE_BUS_CONNECTION_STRING \
---from-literal=SERVICE_BUS_QUEUE_NAME=$SERVICE_BUS_QUEUE_NAME \
+--from-literal=AZURE_WEBPUBSUB_CONNECTION_STRING=$AZURE_WEBPUBSUB_CONNECTION_STRING \
 -n data
 
-# Rest of the script
-kubectl apply -f ../yaml/ingest-environment.yaml
+
